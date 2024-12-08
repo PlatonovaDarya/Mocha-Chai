32a6ba3d82de4c3f004e8351c55fb7a6b6222349
PlatonovaDarya <platonova_d@icloud.com>
2024-12-08 20:46:09 +0300

    Add files via upload

---
+++ test/userService.test.js
@@ -0,0 +1,43 @@
+
+import fetchUsers from '../userService.mjs';
+import { assert, expect } from 'chai';
+import sinon from 'sinon';
+
+describe('Тестирование асинхронной функции fetchUsers', () => {
+  let result;
+  it('Проверка результата при статусе 200', async () => {
+    result = await fetchUsers();
+    expect(result).to.equal(
+      'Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, Chelsey Dietrich, Mrs. Dennis Schulist, Kurtis Weissnat, Nicholas Runolfsdottir V, Glenna Reichert, Clementina DuBuque'
+    );
+  });
+  it('Функция fetchUsers возвращает string', () => {
+    assert.typeOf(result, 'string', 'функция fetchUsers возвращает string');
+  });
+});
+
+describe('Тестирование функции showResult с помощью создания заглушки для функции fetchUsers', () => {
+  const obj = {
+    showResult: async () => await fetchUsers(),
+  };
+  before(function () {
+    const stub = sinon
+      .stub(obj, 'showResult')
+      .returns(
+        'Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, Chelsey Dietrich, Mrs. Dennis Schulist, Kurtis Weissnat, Nicholas Runolfsdottir V, Glenna Reichert, Clementina DuBuque'
+      );
+  });
+
+  it('Функция showResult проверка результата', () => {
+    const result = obj.showResult();
+    expect(obj.showResult()).to.be.equal(
+      'Leanne Graham, Ervin Howell, Clementine Bauch, Patricia Lebsack, Chelsey Dietrich, Mrs. Dennis Schulist, Kurtis Weissnat, Nicholas Runolfsdottir V, Glenna Reichert, Clementina DuBuque'
+    );
+    expect(result).to.be.a('string');
+  });
+
+  it('Функция showResult возвращает string', () => {
+    const result = obj.showResult();
+    expect(result).to.be.a('string');
+  });
+});
---
+++ userService.mjs
@@ -0,0 +1,18 @@
+export default async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  try {
+    if (res.ok) {
+      const data = await res.json();
+      const result = data
+        .map((user) => user.name)
+        .reduce((acc, curr) => acc + ', ' + curr);
+
+      console.log(result);
+      return result;
+    } else {
+      throw new Error(`Ошибка: ${res.status}`);
+    }
+  } catch (erorr) {
+    console.error(erorr);
+  }
+}
